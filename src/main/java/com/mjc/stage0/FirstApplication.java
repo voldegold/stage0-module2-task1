bff8c0d173e72f86e9de1a19618c73837b8661ac
voldegold <kva1107@gmail.com>
2022-07-27 23:36:21 +0600

    solved

--- src/main/java/com/mjc/stage0/FirstApplication.java
+++ src/main/java/com/mjc/stage0/FirstApplication.java
@@ -2,6 +2,7 @@ package com.mjc.stage0;
 
 public class FirstApplication {
     public static void main(String[] args) {
+        System.out.println("Hello, World!");
         // Write your code here!
     }
 }
